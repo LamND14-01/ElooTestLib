959657c35ed93753bf3eacfe57ebafc4ca8ed22b
LamND <Lamnd.eloo.dev@gmail.com>
2022-11-20 19:35:13 +0700

    no message

---
+++ ElooTestLib/ElooTestLib.h
@@ -0,0 +1,18 @@
+//
+//  ElooTestLib.h
+//  ElooTestLib
+//
+//  Created by Eloo on 20/11/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ElooTestLib.
+FOUNDATION_EXPORT double ElooTestLibVersionNumber;
+
+//! Project version string for ElooTestLib.
+FOUNDATION_EXPORT const unsigned char ElooTestLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ElooTestLib/PublicHeader.h>
+
+
