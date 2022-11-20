3ebdb5a88124445848e87a9569786287c79a395e
LamND <Lamnd.eloo.dev@gmail.com>
2022-11-20 19:47:17 +0700

    no message

---
+++ ElooTestLib/ElooTestLib.swift
@@ -0,0 +1,22 @@
+//
+//  ElooTestLib.swift
+//  ElooTestLib
+//
+//  Created by Eloo on 20/11/2022.
+//
+
+import Foundation
+
+public final class ElooTestLib {
+
+    let name = "ElooTestLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
